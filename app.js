8df7f5d1712e380631a8fc1e0d51417bdfb9a9e5
Reshad Noorzay <rnoorzay@gmail.com>
2013-04-10 11:46:39 -0700

    first commit, console chess

---
+++ app.js
@@ -0,0 +1,52 @@
+var turn = true;
+var xCurPos, yCurPos, xNewPos, yNewPos, tempXPos, tempYPos;
+
+var board = 
+[ ['R','N','B','Q','K','B','N','R'],
+  ['P','P','P','P','P','P','P','P'],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  [' ',' ',' ',' ',' ',' ',' ',' '],
+  ['p','p','p','p','p','p','p','p'],
+  ['r','n','b','q','k','b','n','r']];
+
+var showBoard = function(){
+  console.log('\n\n' + board.join('\n') + '\n\n');  
+};
+
+var move = function(){
+  if(turn){
+    console.log("Your move WHITE");
+    makeMove();
+    turn = false;
+  } else {
+    console.log("Your move BLACK");
+    makeMove();
+    turn = true;
+  }
+};
+
+var makeMove = function () {
+  // request moves
+  xCurPos =  parseInt(prompt("PIECE to move, ROW (0-7): "), 10);
+  if(xCurPos){
+    yCurPos = parseInt(prompt("PIECE to move, COLUMN (0-7): "), 10);
+  }
+  if(xCurPos && yCurPos){
+    xNewPos = tempXPos = parseInt(prompt("WHERE to move, ROW (0-7): "), 10);
+  yNewPos = tempYPos = parseInt(prompt("WHERE to move, COLUMN (0-7): "), 10);
+  }
+  // if positions are entered then move piece
+  if(xCurPos && yCurPos && xNewPos && yNewPos){
+    board[xNewPos][yNewPos] = board[xCurPos][yCurPos];
+    board[xCurPos][yCurPos] = ' ';
+  }
+};
+
+// testing more than 1 move
+for(var i = 0; i < 2; i++){
+  showBoard();
+  move();
+  showBoard();
+}
